eaca4f05af8e0caf84ab553b54ed6e53d4c7679f
Divya Maheshwari <60094383+divya814@users.noreply.github.com>
2021-01-08 22:40:03 +0530

    Create Reverse_the_array.py

---
+++ Reverse_the_array.py
@@ -0,0 +1,14 @@
+// reversing the array using swap
+
+n=int(input())
+arr=list(map(int,input().split()))
+st=0
+en=n-1
+while st<en:
+    arr[st],arr[en]=arr[en],arr[st]
+    st+=1
+    en-=1
+print(arr)
+
+
+// or simple- arr[::-1]
