054f48a808bd0ccb6263cfb90ba46e717873ecdd
Divya Maheshwari <60094383+divya814@users.noreply.github.com>
2021-05-03 18:54:35 +0530

    Leetcode

---
+++ remove-linked-list-elements.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/remove-linked-list-elements/
+
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        if head==None:
+            return head
+        curr=head
+        prev=None
+        while curr:
+            if curr.val==val:
+                if curr==head:
+                    head=head.next
+                    curr=head
+                else:
+                    prev.next=curr.next
+                    curr=prev.next
+            else:
+                prev=curr
+                curr=curr.next
+        return head
