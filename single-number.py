6652a11189a2980809650926de893ece74f6b64c
Divya Maheshwari <60094383+divya814@users.noreply.github.com>
2021-05-06 00:30:30 +0530

    Bit Manipulation

---
+++ single-number.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/single-number/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        n=0
+        for i in nums:
+            n=n^i
+        return n
+        
+        
