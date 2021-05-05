7ef9730f74f4e4f41ba6b60cda45807cfe192e6b
Divya Maheshwari <60094383+divya814@users.noreply.github.com>
2021-05-06 02:39:10 +0530

    Counter

---
+++ single-number-ii.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/single-number-ii/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        freq=Counter(nums)
+        for i in freq:
+            if freq[i]==1:
+                return i
+        
+        
