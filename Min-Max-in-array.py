9e09d69f0e00e740f837023c4f213b4f273e6f86
Divya Maheshwari <60094383+divya814@users.noreply.github.com>
2021-07-14 01:06:54 +0530

    Create Min-Max-in-array.py

---
+++ Min-Max-in-array.py
@@ -0,0 +1,26 @@
+# Print minimum and maximum element in an array
+
+class pair:
+    def __init__(self):
+        self.minE=0;
+        self.maxE=0;
+
+def minmax(a):
+    num=pair()
+    if len(a)<=0:
+        num.minE=a[0]
+        num.maxE=a[0]
+        return num
+    num.minE=a[0]
+    num.maxE=a[0]
+    for i in range(0,len(a)):
+        if a[i]<num.minE:
+            num.minE=a[i]
+        if a[i]>num.maxE:
+            num.maxE=a[i]
+    return num
+
+a=[2,1,3,5,4,6,334,43535,4,36,46,35,46,575,67568]
+k=minmax(a)
+print(k.minE)
+print(k.maxE)
